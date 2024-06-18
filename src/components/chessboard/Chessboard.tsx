5d6d95b25d37c558af02520dc5cd8eb8384cdb52
Jobjorn <672641@stud.hvl.no>
2024-06-18 12:25:28 +0200

    Implemented snapgrid and the functions to setup position from FEN string, as well as return the current position as FEN string

--- src/components/chessboard/Chessboard.tsx
+++ src/components/chessboard/Chessboard.tsx
@@ -1,7 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Chessboard.css';
 import Tile from '../tile/Tile';
-import { off } from 'process';
 
 const horizontal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const vertical = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -12,45 +11,56 @@ interface Piece {
     y: number;
 }
 
-const pieces: Piece[] = [];
+const initialBoardState: Piece[] = [];
 
 
 for(let p = 0; p<2; p++){
     const type = p === 0 ? 'd' : 'l';
     const y = p === 0 ? 7 : 0;
     
-    pieces.push({image: `./assets/images/Chess_r${type}t60.png`, x: 0, y})
-    pieces.push({image: `./assets/images/Chess_n${type}t60.png`, x: 1, y})
-    pieces.push({image: `./assets/images/Chess_b${type}t60.png`, x: 2, y})
-    pieces.push({image: `./assets/images/Chess_q${type}t60.png`, x: 3, y})
-    pieces.push({image: `./assets/images/Chess_k${type}t60.png`, x: 4, y})
-    pieces.push({image: `./assets/images/Chess_b${type}t60.png`, x: 5, y})
-    pieces.push({image: `./assets/images/Chess_n${type}t60.png`, x: 6, y})
-    pieces.push({image: `./assets/images/Chess_r${type}t60.png`, x: 7, y})
+    initialBoardState.push({image: `./assets/images/Chess_r${type}t60.png`, x: 0, y})
+    initialBoardState.push({image: `./assets/images/Chess_n${type}t60.png`, x: 1, y})
+    initialBoardState.push({image: `./assets/images/Chess_b${type}t60.png`, x: 2, y})
+    initialBoardState.push({image: `./assets/images/Chess_q${type}t60.png`, x: 3, y})
+    initialBoardState.push({image: `./assets/images/Chess_k${type}t60.png`, x: 4, y})
+    initialBoardState.push({image: `./assets/images/Chess_b${type}t60.png`, x: 5, y})
+    initialBoardState.push({image: `./assets/images/Chess_n${type}t60.png`, x: 6, y})
+    initialBoardState.push({image: `./assets/images/Chess_r${type}t60.png`, x: 7, y})
     
     for(let i =0 ; i<8 ; i++){
-        pieces.push({image: `./assets/images/Chess_p${type}t60.png`, x: i, y: p === 0 ? 6 : 1})
+        initialBoardState.push({image: `./assets/images/Chess_p${type}t60.png`, x: i, y: p === 0 ? 6 : 1})
     }
     
 }
 
 
+
+
 export default function Chessboard() {
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
+    const [gridX, setGridX] = useState(0);
+    const [gridY, setGridY] = useState(0);
     const chessBoardRef = useRef<HTMLDivElement>(null);
+    const [pieces, setPieces] = useState<Piece[]>(initialBoardState);
     
     let board = [];
-    let activePiece: HTMLElement | null = null;
     
     function grabPiece(e: React.MouseEvent){
+        const chessBoard = chessBoardRef.current;
         const target = e.target as HTMLElement;
-        if(target.classList.contains('chess-piece')){
-            const x = e.clientX - 25;
-            const y = e.clientY - 25;
+        if(target.classList.contains('chess-piece') && chessBoard){
+            const gridX = Math.floor((e.clientX - chessBoard.offsetLeft) / 100);
+            const gridY = Math.abs(Math.ceil((e.clientY - chessBoard.offsetTop - 800) / 100));
+            setGridX(gridX);
+            setGridY(gridY);
+
+            const x = e.clientX - 50;
+            const y = e.clientY - 50;
             target.style.position = 'absolute';
             target.style.left = `${x}px`;
             target.style.top = `${y}px`;
     
-            activePiece = target;
+            setActivePiece(target);
         }
     }
     
@@ -59,10 +69,10 @@ export default function Chessboard() {
         if(activePiece && chessBoard){
             const minX = chessBoard.offsetLeft - 25;
             const minY = chessBoard.offsetTop - 25;
-            const maxX = chessBoard.offsetLeft + chessBoard.clientWidth - 25;
-            const maxY = chessBoard.offsetTop + chessBoard.clientHeight - 25;
-            const x = e.clientX - 25;
-            const y = e.clientY - 25;
+            const maxX = chessBoard.offsetLeft + chessBoard.clientWidth - 75;
+            const maxY = chessBoard.offsetTop + chessBoard.clientHeight - 75;
+            const x = e.clientX - 50;
+            const y = e.clientY - 50;
             
             if(x < minX){
                 activePiece.style.left = `${minX}px`;
@@ -81,14 +91,94 @@ export default function Chessboard() {
             }
         }
     }
-    
+
     function dropPiece(e: React.MouseEvent){
-        if(activePiece){
-            activePiece = null;
+        const chessBoard = chessBoardRef.current;
+
+        if(activePiece && chessBoard){
+            const x = Math.floor((e.clientX - chessBoard.offsetLeft) / 100);
+            const y = Math.abs(Math.ceil((e.clientY - chessBoard.offsetTop - 800) / 100));
+            console.log(x, y);
+            setPieces((value) => {
+            const pieces = value.map(p => {
+                if(p.x === gridX && p.y === gridY){
+                    p.x = x;
+                    p.y = y;
+                }
+                return p;       
+        });
+        return pieces;
+        });
+        setActivePiece(null);
+    }}
+
+
+    function setUpPositionFromFEN(FEN: string){
+        const fen = FEN;
+        const rows = fen.split('/');
+        const pieces: Piece[] = [];
+        
+        for(let i = 0; i<rows.length; i++){
+            const row = rows[i];
+            let x = 0;
+            for(let j = 0; j<row.length; j++){
+                const char = row[j];
+                if(isNaN(parseInt(char))){
+                    let isLowerCase = char === char.toLowerCase();
+                    if(isLowerCase){
+                        pieces.push({image: `./assets/images/Chess_${char}dt60.png`, x, y: 7 - i})
+                    }
+                    else {
+                        const lightChar = char.toLowerCase();
+                        pieces.push({image: `./assets/images/Chess_${lightChar}lt60.png`, x, y: 7 - i})
+                    }
+                    x++;
+                } else {
+                    x += parseInt(char);
+                }
+            }
         }
-    
+        setPieces(pieces);
     }
-    
+
+    function getFENFromPosition(){
+    let fen = '';
+    for(let i = 7; i>=0; i--){
+        let empty = 0;
+        for(let j = 0; j<8; j++){
+            let piece = '';
+            pieces.forEach(p => {
+                if(p.x === j && p.y === i){
+                    const pieceType = p.image.split('_')[1].split('t')[0][0];
+                    const pieceColor = p.image.split('_')[1].split('t')[0][1]
+                    if(pieceColor === 'l'){
+                        piece = pieceType.toUpperCase();
+                    }else{
+                        piece = pieceType;
+                    }
+                }
+            });
+            if(piece){
+                if(empty){
+                    fen += empty;
+                    empty = 0;
+                }
+                fen += piece;
+            } else {
+                empty++;
+            }
+        }
+        if(empty){
+            fen += empty;
+        }
+        if(i > 0){
+            fen += '/';
+        }
+    }
+    console.log(fen);}
+
+
+    // Create the board
     for (let i = vertical.length-1; i >= 0; i--) {
         for (let j = 0; j < horizontal.length; j++) {
             const number = i + j + 2;
@@ -103,19 +193,20 @@ export default function Chessboard() {
             board.push(<Tile key={`${j}, ${i}`} image={image} number={number}/>
         );
     }
-    }
+}
 
+    // Render the board
     return (
-  
         <div 
         onMouseUp={e=> dropPiece(e)} 
         onMouseMove={e => movePiece(e)} 
         onMouseDown={e => grabPiece(e)} 
         id="chessboard"
-        ref={chessBoardRef}
-        >
-
-
+        ref={chessBoardRef}>   
             {board}
-        </div>)
+
+            <button onClick={() => setUpPositionFromFEN('1r3r2/2qn2k1/p2p2pp/2pPp3/2P1P3/1P3NPP/1B1Q1PK1/3R1R2')}>Set up position</button>
+            <button onClick={() => getFENFromPosition()}>Get FEN</button>
+        </div>   
+    )
 }
