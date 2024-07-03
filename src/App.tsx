99fd5d37f06ec41a083104042754d7004a01d1ef
Jobjorn <672641@stud.hvl.no>
2024-07-03 09:12:34 +0200

    added home navigation when pressing company in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';import './App.css';
-import Chessboard from './components/chessboard/Chessboard';
 import io, { Socket } from 'socket.io-client';
-import Header from './components/header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import { SocketProvider } from './socket/SocketContext';
 import Selection from './pages/selection/Selection';
-import { NonNullChain } from 'typescript';
+
 
 function App() {
   const[socketInstance, setSocketInstance] = useState<Socket | null>(null);
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Header() {
+  const navigate = useNavigate()
+  const handleCompanyPressed = () => {
+    navigate('/')
+  }
   return (
     <header id="header">
-      <div id="company">
+      <div id="company" onClick={handleCompanyPressed}>
         hvl<span className="bold">robotics</span>
       </div>
       <div id="about">about us</div>
